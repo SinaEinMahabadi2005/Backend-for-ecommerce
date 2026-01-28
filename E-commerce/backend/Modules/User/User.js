7a1bc575760a4a3f90e93d500b22286e4528b0a7
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-01-29 01:01:45 +0330

    add route for change password

--- E-commerce/backend/Modules/User/User.js
+++ E-commerce/backend/Modules/User/User.js
@@ -1,19 +1,28 @@
 import { Router } from "express";
 import isAdmin from "../../Middlewares/isAdmin.js";
 import {
+  changePasswordValidator,
   getAllUserValidator,
   updateUserValidator,
   userIdParam,
 } from "./UserValidator.js";
 import { handleValidationErrors } from "../../Utils/handleValidationError.js";
-import { getAll, getOne, update } from "./UserCn.js";
+import { changePassword, getAll, getOne, update } from "./UserCn.js";
 import isLogin from "../../Middlewares/isLogin.js";
 const userRouter = Router();
 userRouter
   .route("/")
   .get(isAdmin, getAllUserValidator, handleValidationErrors, getAll);
 userRouter
+  .route("/change-password")
+  .patch(
+    isLogin,
+    changePasswordValidator,
+    handleValidationErrors,
+    changePassword,
+  );
+userRouter
   .route("/:id")
   .get(isLogin, userIdParam, handleValidationErrors, getOne)
   .patch(isLogin, updateUserValidator, handleValidationErrors, update);
-  export default userRouter
+export default userRouter;
