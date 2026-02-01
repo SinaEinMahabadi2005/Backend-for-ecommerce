2aab4db4967d30a32cec414554100528ad23c656
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-01 20:20:50 +0330

    add login with otp controllers

--- E-commerce/backend/Modules/Auth/AuthCn.js
+++ E-commerce/backend/Modules/Auth/AuthCn.js
@@ -1,8 +1,9 @@
 import { catchAsync, HandleERROR } from "vanta-api";
 import User from "../User/UserMd.js";
-import { sendAuthCode } from "../../Utils/smsHandler.js";
+import { sendAuthCode, verifyCode } from "../../Utils/smsHandler.js";
 import bcryptjs from "bcryptjs"
 import jwt from "jsonwebtoken"
+import Cart from "../Cart/CartMd.js";
 export const auth = catchAsync(async (req, res, next) => {
   const { phoneNumber } = req.body;
   const user = await User.findOne({ phoneNumber });
@@ -38,7 +39,7 @@ export const loginWithPassword = catchAsync(async (req, res, next) => {
   const token=jwt.sign({_id:user._id , role:user.role},process.env.JWT_SECRET)
   return res.status(200).json({
     success:true ,
-    message:"login successfully" ,
+    message:"login with password successfully" ,
     token ,
     user:{
       _id:user._id ,
@@ -50,3 +51,33 @@ export const loginWithPassword = catchAsync(async (req, res, next) => {
   })
 
 });
+//
+export const loginWithOtp = catchAsync(async (req, res, next) => {
+  const {phoneNumber , code}=req.body
+ const resultVerify=await verifyCode(phoneNumber,code)
+ if(!resultVerify){
+  return next(new HandleERROR("invalid code",400))
+ }
+  const user=await User.findOne({phoneNumber}).populate("cartId")
+  if(!user){
+    user=await User.create({phoneNumber})
+    const cartId=await Cart.create({userId:user._id})
+    user.cartId=cartId._id
+    await user.save()
+  }
+  const token=jwt.sign({_id:user._id , role:user.role},process.env.JWT_SECRET)
+  return res.status(200).json({
+    success:true ,
+    message:"login with otp successfully" ,
+    token ,
+    user:{
+      _id:user._id ,
+      role:user.role ,
+      fullName:user.fullName ,
+      phoneNumber:user.phoneNumber ,
+      cartId:user.cartId
+    }
+  })
+
+});
+
