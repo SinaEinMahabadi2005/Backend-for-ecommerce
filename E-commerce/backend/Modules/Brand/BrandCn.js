a4fbd19b65d27d7cae489b759574bed0556cff84
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-02 00:56:11 +0330

    add get one controllers for brand

--- E-commerce/backend/Modules/Brand/BrandCn.js
+++ E-commerce/backend/Modules/Brand/BrandCn.js
@@ -1,5 +1,6 @@
 import ApiFeatures, { catchAsync } from "vanta-api";
 import Brand from "./BrandMd.js";
+// get all
 export const getAll = catchAsync(async (req, res, next) => {
   const feature = new ApiFeatures(Brand, req.query, req.role)
     .addManualFilters(
@@ -15,3 +16,23 @@ export const getAll = catchAsync(async (req, res, next) => {
   const result = await feature.execute();
   res.status(200).json(result);
 });
+// get one
+export const getOne = catchAsync(async (req, res, next) => {
+  const feature = new ApiFeatures(Brand, req.query, req.role)
+    .addManualFilters(
+      req.role == "admin" || req.role == "superAdmin"
+        ? { _id: req.params.id }
+        : { $and: [{ isPublished: true }, { _id: req.params.id }] },
+    )
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate()
+    .populate();
+  const result = await feature.execute();
+  res.status(200).json(result);
+});
+//create
+export const create=catchAsync(async (req,res,next) => {
+    const brand=await Brand.create(req.body)
+})
