b7ec4185b0cb8a58bd46951be682f5493787f2b0
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-01 20:43:11 +0330

    add resend code controllers

--- E-commerce/backend/Modules/Auth/AuthCn.js
+++ E-commerce/backend/Modules/Auth/AuthCn.js
@@ -1,8 +1,8 @@
 import { catchAsync, HandleERROR } from "vanta-api";
 import User from "../User/UserMd.js";
 import { sendAuthCode, verifyCode } from "../../Utils/smsHandler.js";
-import bcryptjs from "bcryptjs"
-import jwt from "jsonwebtoken"
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import Cart from "../Cart/CartMd.js";
 export const auth = catchAsync(async (req, res, next) => {
   const { phoneNumber } = req.body;
@@ -26,58 +26,75 @@ export const auth = catchAsync(async (req, res, next) => {
   });
 });
 export const loginWithPassword = catchAsync(async (req, res, next) => {
-  const {phoneNumber , password}=req.body
+  const { phoneNumber, password } = req.body;
   // complete populate cartId
-  const user=await User.findOne({phoneNumber}).populate("cartId")
-  if(!user || !user?.password){
-    return next(new HandleERROR("invalid phoneNumber Or Password",400))
+  const user = await User.findOne({ phoneNumber }).populate("cartId");
+  if (!user || !user?.password) {
+    return next(new HandleERROR("invalid phoneNumber Or Password", 400));
   }
-  const isMatch=bcryptjs.compareSync(password,user.password)
-  if(!isMatch){
-    return next(new HandleERROR("invalid phoneNumber Or Password",400))
+  const isMatch = bcryptjs.compareSync(password, user.password);
+  if (!isMatch) {
+    return next(new HandleERROR("invalid phoneNumber Or Password", 400));
   }
-  const token=jwt.sign({_id:user._id , role:user.role},process.env.JWT_SECRET)
+  const token = jwt.sign(
+    { _id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+  );
   return res.status(200).json({
-    success:true ,
-    message:"login with password successfully" ,
-    token ,
-    user:{
-      _id:user._id ,
-      role:user.role ,
-      fullName:user.fullName ,
-      phoneNumber:user.phoneNumber ,
-      cartId:user.cartId
-    }
-  })
-
+    success: true,
+    message: "login with password successfully",
+    token,
+    user: {
+      _id: user._id,
+      role: user.role,
+      fullName: user.fullName,
+      phoneNumber: user.phoneNumber,
+      cartId: user.cartId,
+    },
+  });
 });
 //
 export const loginWithOtp = catchAsync(async (req, res, next) => {
-  const {phoneNumber , code}=req.body
- const resultVerify=await verifyCode(phoneNumber,code)
- if(!resultVerify){
-  return next(new HandleERROR("invalid code",400))
- }
-  const user=await User.findOne({phoneNumber}).populate("cartId")
-  if(!user){
-    user=await User.create({phoneNumber})
-    const cartId=await Cart.create({userId:user._id})
-    user.cartId=cartId._id
-    await user.save()
+  const { phoneNumber, code } = req.body;
+  const resultVerify = await verifyCode(phoneNumber, code);
+  if (!resultVerify) {
+    return next(new HandleERROR("invalid code", 400));
   }
-  const token=jwt.sign({_id:user._id , role:user.role},process.env.JWT_SECRET)
+  const user = await User.findOne({ phoneNumber }).populate("cartId");
+  if (!user) {
+    user = await User.create({ phoneNumber });
+    const cartId = await Cart.create({ userId: user._id });
+    user.cartId = cartId._id;
+    await user.save();
+  }
+  const token = jwt.sign(
+    { _id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+  );
   return res.status(200).json({
-    success:true ,
-    message:"login with otp successfully" ,
-    token ,
-    user:{
-      _id:user._id ,
-      role:user.role ,
-      fullName:user.fullName ,
-      phoneNumber:user.phoneNumber ,
-      cartId:user.cartId
-    }
-  })
-
+    success: true,
+    message: "login with otp successfully",
+    token,
+    user: {
+      _id: user._id,
+      role: user.role,
+      fullName: user.fullName,
+      phoneNumber: user.phoneNumber,
+      cartId: user.cartId,
+    },
+  });
 });
-
+export const resendCode = catchAsync(async (req, res, next) => {
+  const { phoneNumber } = req.body;
+  const resultSms = await sendAuthCode(phoneNumber);
+  if (!resultSms) {
+    return res.status(500).json({
+      success: false,
+      message: resultSms.message,
+    });
+  }
+  return res.status(200).json({
+    success: true,
+    message: "otp code sent",
+  });
+});
