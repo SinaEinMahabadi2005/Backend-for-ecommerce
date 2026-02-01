83e5ad792e7f38e84f172d11e224c12a65cf3d69
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-02 01:44:54 +0330

    add remove brand functionality with error handling for related products

--- E-commerce/backend/Modules/Brand/BrandCn.js
+++ E-commerce/backend/Modules/Brand/BrandCn.js
@@ -1,5 +1,8 @@
-import ApiFeatures, { catchAsync } from "vanta-api";
+import ApiFeatures, { catchAsync, HandleERROR } from "vanta-api";
 import Brand from "./BrandMd.js";
+import Product from "../Product/ProductMd.js";
+import fs from "fs";
+import __dirname from "./../../app.js";
 // get all
 export const getAll = catchAsync(async (req, res, next) => {
   const feature = new ApiFeatures(Brand, req.query, req.role)
@@ -53,4 +56,23 @@ export const update = catchAsync(async (req, res, next) => {
     data: brand,
   });
 });
-//
+//remove
+export const remove = catchAsync(async (req, res, next) => {
+  const product = await Product.find({ brandId: req.params.id });
+  if (product.length > 0) {
+    return next(
+      new HandleERROR(
+        "You can not delete this brand because there are products related to it",
+        400,
+      ),
+    );
+  }
+  const brand = await Brand.findByIdAndDelete(req.params.id);
+  if (fs.existsSync(`${__dirname}/Public/${brand.image}`)) {
+    fs.unlinkSync(`${__dirname}/Public/${brand.image}`);
+  }
+  return res.status(200).json({
+    success:true ,
+    message:"brand deleted successfully"
+  })
+});
