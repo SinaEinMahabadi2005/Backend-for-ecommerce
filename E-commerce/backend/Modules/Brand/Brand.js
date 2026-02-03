9302e3df784fd160b59e76a9f7b89aafd625492c
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-03 19:31:34 +0330

    refactor brand routes to include validation and error handling

--- E-commerce/backend/Modules/Brand/Brand.js
+++ E-commerce/backend/Modules/Brand/Brand.js
@@ -1,11 +1,21 @@
 import { Router } from "express";
 import { create, getAll, getOne, remove, update } from "./BrandCn.js";
 import isAdmin from "../../Middlewares/isAdmin.js";
+import {
+  brandIdParam,
+  createBrandValidator,
+  getAllBrandValidator,
+  updateBrandValidator,
+} from "./BrandValidator.js";
+import { handleValidationErrors } from "../../Utils/handleValidationError.js";
 const brandRouter = Router();
-brandRouter.route("/").get(getAll).post(isAdmin, create);
+brandRouter
+  .route("/")
+  .get(getAllBrandValidator, handleValidationErrors, getAll)
+  .post(isAdmin, createBrandValidator, handleValidationErrors, create);
 brandRouter
   .route("/:id")
-  .get(getOne)
-  .patch(isAdmin, update)
-  .delete(isAdmin, remove);
+  .get(brandIdParam, handleValidationErrors, getOne)
+  .patch(isAdmin, updateBrandValidator, handleValidationErrors, update)
+  .delete(isAdmin, brandIdParam, handleValidationErrors, remove);
 export default brandRouter;
