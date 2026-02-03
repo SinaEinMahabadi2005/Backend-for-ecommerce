8bb32a7344e88fee7cb97cd38300de733b0a8cf4
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-04 00:15:53 +0330

    update category router

--- E-commerce/backend/Modules/Category/Category.js
+++ E-commerce/backend/Modules/Category/Category.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { create, getAll, getOne, remove, update } from "./CategoryCn.js";
 import isAdmin from "../../Middlewares/isAdmin.js";
+import { categoryIdParam, createCategoryValidator, getAllCategoryValidator, updateCategoryValidator } from "./CategoryValidator.js";
+import { handleValidationErrors } from "../../Utils/handleValidationError.js";
 const categoryRouter = Router();
-categoryRouter.route("/").get(getAll).post(isAdmin, create);
+categoryRouter.route("/").get(getAllCategoryValidator,handleValidationErrors,getAll).post(isAdmin,createCategoryValidator,handleValidationErrors, create);
 categoryRouter
   .route("/:id")
-  .get(getOne)
-  .patch(isAdmin, update)
-  .delete(isAdmin, remove);
+  .get(categoryIdParam,handleValidationErrors,getOne)
+  .patch(isAdmin,updateCategoryValidator,handleValidationErrors, update)
+  .delete(isAdmin,categoryIdParam,handleValidationErrors, remove);
 export default categoryRouter
