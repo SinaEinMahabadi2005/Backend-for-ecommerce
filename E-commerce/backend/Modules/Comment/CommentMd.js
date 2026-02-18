3236fdf9597eb2814983c82cf6bba111b618b1b0
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-18 09:56:39 +0330

    add comment model

--- E-commerce/backend/Modules/Comment/CommentMd.js
+++ E-commerce/backend/Modules/Comment/CommentMd.js
@@ -1,17 +1,45 @@
 import mongoose from "mongoose";
-const commentSchema = new mongoose.Schema({
-  
-  productId:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:'Product',
-    default:null
+const commentSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      default: null,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "user id is required"],
+    },
+    content: {
+      type: String,
+      required: [true, "content is required"],
+      trim: true,
+    },
+    reply: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+    isReply: {
+      type: Boolean,
+      default: false,
+    },
+    rate: {
+      min: 0,
+      max: 0,
+      type: Number,
+    },
+    isBought: {
+      type: Boolean,
+      default: false,
+    },
+    isPublished: {
+      type: Boolean,
+      default: false,
+    },
   },
-
-  isPublished:{
-    type:Boolean,
-    default:true
-  }
-},{timestamps:true});
+  { timestamps: true },
+);
 
 const Comment = mongoose.model("Comment", commentSchema);
 export default Comment;
