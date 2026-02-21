2703aa03c66775aba389b5f4861fa9dd8573581e
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-21 20:31:47 +0330

    finish the cart model

--- E-commerce/backend/Modules/Cart/CartMd.js
+++ E-commerce/backend/Modules/Cart/CartMd.js
@@ -1,10 +1,48 @@
 import mongoose from "mongoose";
+const itemSchema=new mongoose.Schema({
+  productId:{
+    type:mongoose.Schema.Types.ObjectId ,
+    ref:"Product" ,
+    required:[true ,"product id is required"]
+  } ,
+   productVariantId:{
+    type:mongoose.Schema.Types.ObjectId ,
+    ref:"ProductVariant" ,
+    required:[true ,"ProductVariant id is required"]
+  } ,
+   categoryId:{
+    type:mongoose.Schema.Types.ObjectId ,
+    ref:"Category" ,
+    required:[true ,"category id is required"]
+  } ,
+   brandId:{
+    type:mongoose.Schema.Types.ObjectId ,
+    ref:"Brand" ,
+    required:[true ,"brand id is required"]
+  } ,
+  quantity:{
+    type:Number ,
+    default:0
+  }
+},{_id:false})
 const cartSchema = new mongoose.Schema(
   {
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
+    items:{
+      type:[itemSchema] ,
+      default:[]
+    } ,
+    totalPrice:{
+      type:Number ,
+      default:0
+    } ,
+     totalPriceAfterDiscount:{
+      type:Number ,
+      default:0
+    }
   },
   { timestamps: true },
 );
