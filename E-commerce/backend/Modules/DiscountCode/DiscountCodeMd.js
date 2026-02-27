816c01f3baeb93e2a9e6a4378216cacb7945b63b
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-27 15:30:13 +0330

    Add DiscountCode Model

---
+++ E-commerce/backend/Modules/DiscountCode/DiscountCodeMd.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+const discountCodeSchema = new mongoose.Schema(
+  {
+    code: {
+      type: String,
+      required: [true, "code is required"],
+      unique: [true, "code is unique"],
+      trim: true,
+    },
+    maxPrice: {
+      type: Number,
+    },
+    minPrice: {
+      type: Number,
+    },
+    type: {
+      type: String,
+      enum: ["percent", "amount"],
+      required: [true, "type is required"],
+    },
+    startDate: {
+      type: Date,
+    },
+    endDate: {
+      type: Date,
+    },
+    value: {
+      type: String,
+      required: [true, "value is required"],
+    },
+    maxUsedCount: {
+      type: Number,
+      default: 1,
+    },
+    userIdsUsed: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+      ],
+      default: [],
+    },
+    freeShipping: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+const DiscountCode = mongoose.model("DiscountCode", discountCodeSchema);
+export default DiscountCode;
