d26048dee6373eb65868ecf968b409dffb20d424
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-27 17:01:52 +0330

    add discountCode controllers

---
+++ E-commerce/backend/Modules/DiscountCode/DiscountCodeCn.js
@@ -0,0 +1,70 @@
+import ApiFeatures, { catchAsync, HandleERROR } from "vanta-api";
+import DiscountCode from "./DiscountCodeMd.js";
+
+// get all
+export const getAll = catchAsync(async (req, res, next) => {
+  const feature = new ApiFeatures(DiscountCode, req.query, req.role)
+    .addManualFilters(
+      req.query.search
+        ? { code: { $regex: req.query.search, $options: "i" } }
+        : {},
+    )
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate()
+    .populate();
+  const result = await feature.execute();
+  res.status(200).json(result);
+});
+// get one
+export const getOne = catchAsync(async (req, res, next) => {
+  const feature = new ApiFeatures(DiscountCode, req.query, req.role)
+    .addManualFilters({ _id: req.params.id })
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate()
+    .populate({path:"userIdsUsed" , select:"phoneNumber role fullName"});
+  const result = await feature.execute();
+  res.status(200).json(result);
+});
+//create
+export const create = catchAsync(async (req, res, next) => {
+  const discountCode = await DiscountCode.create(req.body);
+  return res.status(200).json({
+    success: true,
+    message: "create discountCode successfully",
+    data: discountCode,
+  });
+});
+//update
+export const update = catchAsync(async (req, res, next) => {
+  const discountCode = await DiscountCode.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  return res.status(200).json({
+    success: true,
+    message: "update discountCode successfully",
+    data: discountCode,
+  });
+});
+//remove
+export const remove = catchAsync(async (req, res, next) => {
+const discountCode=await DiscountCode.findById(req.params.id)
+if(discountCode.userIdsUsed.length > 0){
+    return next(new HandleERROR("you cant delete this discountCode",400))
+}
+await DiscountCode.findByIdAndDelete(req.params.id)
+  return res.status(200).json({
+    success: true,
+    message: "discountCode deleted successfully",
+  });
+});
+export const validateCode=(userId,totalPrice,discountCode)=>{
+    
+}
+export const checkCode=catchAsync(async (req,res,next) => {
+    
+})
